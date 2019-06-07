b14bc145d83fc3ee0227bc64a63d56f36d33d027
Samuel Verschelde <stormi@laposte.net>
2019-06-07 10:18:06 +0200

    Improved prompt with even simpler definitions

--- src/common/etc/profile.d/xcp-ng-prompt.sh
+++ src/common/etc/profile.d/xcp-ng-prompt.sh
@@ -4,7 +4,7 @@
 
 # BASH : Configure things differently if current terminal has >= 8 colors.
 if which tput > /dev/null 2>&1 && [[ $(tput colors) -ge 8 ]]; then
-    PS1=$'[\\[\e[1;38;5;23m\\]\\D{%H:%M} \\[\e[38;5;167m\\]\\h \\[\e[38;5;188m\\]\\W\\[\e[m\\]]\\$ '
+    PS1='[\[\e[1;38;5;23m\]\D{%H:%M} \[\e[38;5;167m\]\h \[\e[m\]\W]\$ '
 else
     PS1='[\D{%H:%M} \h \W]\$ '
 fi
