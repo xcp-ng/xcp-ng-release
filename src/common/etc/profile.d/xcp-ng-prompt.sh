42564362b0702cb6e4da2345132a26d4e23361ef
Samuel Verschelde <stormi@laposte.net>
2019-06-12 18:27:16 +0200

    Check that PS1 exists before trying to change it
    And also
    - mask tput error messages
    - use [] instead of [[]] to avoid a bashism
    - no need to test for the existence of tput, if it doesn't exist the
    command will just silently fail

--- src/common/etc/profile.d/xcp-ng-prompt.sh
+++ src/common/etc/profile.d/xcp-ng-prompt.sh
@@ -3,7 +3,7 @@
 ########################################
 
 # BASH : Configure things differently if current terminal has >= 8 colors.
-if which tput > /dev/null 2>&1 && [[ $(tput colors) -ge 8 ]]; then
+if [ "$PS1" ] && [ $(tput colors 2>/dev/null) -ge 8 ]; then
     PS1='[\[\e[1;38;5;23m\]\D{%H:%M} \[\e[38;5;167m\]\h \[\e[m\]\W]\$ '
 else
     PS1='[\D{%H:%M} \h \W]\$ '
